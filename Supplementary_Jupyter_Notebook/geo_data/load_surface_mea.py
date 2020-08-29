a8c5d70d904fd624c1bf8f662475348131ff680d
brennanbrunsvik <brennan.brunsvik@gmail.com>
2020-08-28 18:37:36 -0700

    Plotly fault trace image progress
    Fault trace colors improved. Added plotly legend. Other changes...

---
+++ Supplementary_Jupyter_Notebook/geo_data/load_surface_mea.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 25 10:11:51 2020
+
+@author: brennanbrunsvik
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+fname = 'surface_meas_boncio'
+
+sm = pd.read_table(fname)
+no_meas = np.isnan(sm.Lat)
+sm = sm[~no_meas]
+nostrike = sm.strike == '-'
+sm.strike[nostrike] = np.nan
+sm.strike = sm.strike.astype(float)
+
+# plt.scatter(sm.Long, sm.Lat)
+pag = sm.code
+
+# plt.hist(sm.strike)
+# plt.show()
+
+plt.scatter(sm.Long, sm.Lat, c = sm.strike, vmin = 90, vmax = 160)
+plt.colorbar()
